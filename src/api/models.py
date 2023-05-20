30ce49b094befebf971033e3b36b6a4338e92229
Nico <nicosis@gmail.com>
2023-05-20 10:03:47 +0000

    work done

--- src/api/models.py
+++ src/api/models.py
@@ -25,7 +25,7 @@ class Student(db.Model):
     programming_skills = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<Student {self.name_student}>'
+        return f'Student {self.name_student}'
 
     def serialize(self):
         return {
@@ -33,4 +33,40 @@ class Student(db.Model):
             "name_student": self.name_student,
             "email": self.email,
             "programming_skills": self.programming_skills,
+        }
+    
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(200), unique=True, nullable=False)
+    topics = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'Project {self.project_name}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "project_name": self.project_name,
+            "topics": self.topics,
+        }
+
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    submited_date = db.Column(db.Date, nullable=False)
+
+    project = db.relationship('Project', backref='submissions')
+    student = db.relationship('Student', backref='submissions')
+
+    def __repr__(self):
+        return f'Submission: {self.student.name_student} / {self.submited_date }'
+
+    def serialize(self):
+        return {
+            "project_id": self.project_id,
+            "student_id": self.student_id,
+            "submited_date": self.submited_date,
+            "student_name": self.student.name_student,
+            "project_name": self.project.project_name,
         }
--- src/app.py
+++ src/app.py
@@ -7,11 +7,12 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db, Student
+from api.models import db, Student, Project, Submission
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from sqlalchemy import desc
+from datetime import datetime
 
 #from models import Person
 
@@ -64,27 +65,7 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0 # avoid cache memory
     return response
 
-
-@app.route('/student', methods=['GET'])
-def get_student():
-    students = Student.query.all()
-    #all_students = list(map(lambda x: x.serialize(), student))
-    student_data = [{'name_student': student.name_student, 'email': student.email} for student in students]
-    return jsonify(student_data), 200
-
-@app.route('/student/alto', methods=['GET'])
-def get_student_alto_id():
-    #students = Student.query.all()
-    student = Student.query.order_by(desc(Student.id)).first()
-    student_data = {'id': student.id, 'name_student': student.name_student}
-    return jsonify(student_data), 200
-
-@app.route('/student/check', methods=['GET'])
-def get_student_check():
-    students = Student.query.filter_by(programming_skills=True).all()
-    student_data = [student.serialize() for student in students]
-    return jsonify(student_data), 200
-
+# 1.2 Insertar Student
 @app.route('/student', methods=['POST'])
 def add_student():
 
@@ -94,8 +75,99 @@ def add_student():
     db.session.commit()
     return jsonify('student added:',new_student.serialize()), 200
 
+# 1.3 Recupera todos los esutdiantes de la base de datos usando SQLAlchemy. Muestra su nombre y su email
+@app.route('/student', methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    #all_students = list(map(lambda x: x.serialize(), student))
+    student_data = [{'id': student.id, 'name_student': student.name_student, 'email': student.email} for student in students]
+    print(student_data)
+    return jsonify(student_data), 200
+
+# 1.4 Recupera el estudiante cuyo **id** es el más alto de todos. Muestra su **nombre** y su **email**.
+@app.route('/student/alto', methods=['GET'])
+def get_student_alto_id():
+    student = Student.query.order_by(desc(Student.id)).first()
+    student_data = {'id': student.id, 'name_student': student.name_student}
+    print(student_data)
+    return jsonify(student_data), 200
+
+# 1.5 Recupera SOLAMENTE los estudiantes de la base de datos que SÍ tenian conocimientos previos de programación. Muestra su nombre y email. 
+@app.route('/student/check', methods=['GET'])
+def get_student_check():
+    students = Student.query.filter_by(programming_skills=True).all()
+    student_data = [student.serialize() for student in students]
+    print(student_data)
+    return jsonify(student_data), 200
+
+
+# 2.1 Recupera todos los proyectos y muestralos por el terminal
+@app.route('/project', methods=['GET'])
+def get_projects():
+    projects = Project.query.all()
+    all_projects = list(map(lambda x: x.serialize(), projects))
+    print(all_projects)
+    return jsonify(all_projects), 200
+
+# 2.2 Recupera todos los proyectos que tratan sobre el topic “JavaScript”.
+@app.route('/project/js', methods=['GET'])
+def get_project_js():
+    projects = Project.query.filter(Project.topics.like('%JavaScript%')).all()
+    all_projects = [project.serialize() for project in projects]
+    return jsonify(all_projects), 200
+
+# 2.2 Recuperar proyectos en base a un string
+@app.route('/project/<string:palabra>', methods=['GET'])
+def get_project_param(palabra):
+    projects = Project.query.filter(Project.topics.ilike(f'%{palabra}%')).all()
+    all_projects = [project.serialize() for project in projects]
+    print(all_projects)
+    return jsonify(all_projects), 200
+
+# 3.2 Recupera todos los proyectos entregados por Alexander  // <int:student_id>
+@app.route('/project/<int:student_id>', methods=['GET'])
+def get_project_by_student_id(student_id):
+    students = Student.query.get(student_id)
+    all_projects = [project.serialize() for project in students.submissions]
+    print(all_projects)
+    return jsonify(all_projects), 200
+
+# 3.3 Recupera todos los proyectos de todos lo estudiantes cuya fecha de entrega ha sido antes del 17 de Abril de 2023
+@app.route('/submission/<string:date>', methods=['GET'])
+def get_project_before_date(date):
+
+    submissions = Submission.query.filter(Submission.submited_date < date).all()
+    all_subm = [subm.serialize() for subm in submissions]
+    #for all_subm in submissions:
+    print(all_subm)
+    return jsonify(all_subm), 200
+
+# 3.3b Solo para terminal
+def get_project_before_date2(date):
+    submissions = Submission.query.filter(Submission.submited_date < date).all()
+    for submission in submissions:
+        print(submission)
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
+    # Lo necesitamos para pdoer ejecutar código Python normal fuera del entorno de Flask
+    #with app.app_context():
+        #Iteración 1.3
+        #get_students()
+        #Iteración 1.4
+        #get_student_alto_id()
+        #Iteración 1.5
+        #get_student_check()
+        #Iteración 2.1
+        #get_projects()
+        #Iteración 2.2
+        #get_project_param("JavaScript")
+        #Iteración 3.2
+        #get_project_by_student_id(4)
+        #Iteración 3.3
+        #get_project_before_date2(datetime(2023, 5, 7))
+        #get_project_before_date(datetime(2023, 5, 7))
+
